e088eb7212c3f8360561a188a56852e8db320fa2
Reginaldo-Lima <146787952+Reginaldo-Lima@users.noreply.github.com>
2023-10-12 00:41:00 +0000

    correção do arquivo

--- terceiro-desafio.js
+++ terceiro-desafio.js
@@ -1,67 +1,34 @@
-/*
-class hero{
-    constructor(name, age, type){
-        this.name = name
-        this.age = age
-        this.type = type
-    }
-    createAttack(){
-        let attack
-        switch (this.type) {
-            case 'mago':
-        attack = 'usou magia';
-        break;
-      case 'guerreiro':
-        attack = 'usou espada';
-        break;
-      case 'monge':
-        attack = 'usou artes marciais';
-        break;
-      case 'ninja':
-        attack = 'usou shuriken';
-        break;
-      default:
-        attack = 'usou um ataque desconhecido';
-        }
-    }
-
-    toString(){
-        console.log(`o ${this.type} atacou usando ${attack}`)
-    }   
-}    
-*/
-//chatGPT
-class Heroi {
-    constructor(nome, idade, tipo) {
-      this.nome = nome;
-      this.idade = idade;
-      this.tipo = tipo;
-    }
-  
-    atacar() {
-      let ataque = '';
-  
-      switch (this.tipo) {
-        case 'mago':
-          ataque = 'usou magia';
-          break;
-        case 'guerreiro':
-          ataque = 'usou espada';
-          break;
-        case 'monge':
-          ataque = 'usou artes marciais';
-          break;
-        case 'ninja':
-          ataque = 'usou shuriken';
-          break;
-        default:
-          ataque = 'usou um ataque desconhecido';
-      }
-  
-      console.log(`O ${this.tipo} atacou usando ${ataque}`);
-    }
+//Criação da classe heroi
+class Hero {
+  constructor(name, age, type) {
+      this.name = name;
+      this.age = age;
+      this.type = type;
   }
-  
-  // Exemplo de uso da classe
-  const meuHeroi = new Heroi('Herói1', 30, 'mago');
-  meuHeroi.atacar();
+
+  attack() {
+      let attack;
+      switch (this.type) {
+          case "mago":
+              attack = "magia"
+              break
+          case "guerreiro":
+              attack = "espada"
+              break
+          case "monge":
+              attack = "artes marciais"
+              break
+          case "ninja":
+              attack = "shuriken"
+              break
+          default:
+              attack = "usou um ataque desconhecido"
+      }
+
+      const mensagem = `o ${this.type} atacou usando ${attack}`
+      console.log(mensagem)
+  }
+}
+
+const myHero = new Hero("Aragorn", 35, "guerreiro")
+myHero.attack()
